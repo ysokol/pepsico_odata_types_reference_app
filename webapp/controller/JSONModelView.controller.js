25aa04b14fa00d0fe69ed0e3a559e11ff5a56102
S0004431717 <sokolovskij-yurij@yandex.ru>
2018-11-23 15:24:27 +0000

    initital commit

---
+++ webapp/controller/JSONModelView.controller.js
@@ -0,0 +1,9 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("com.pepsico.dev.odataTypes.pepsico_odata_types_reference_app.controller.JSONModelView", {
+
+	});
+});
